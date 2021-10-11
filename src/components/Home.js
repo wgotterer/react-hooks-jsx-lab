d7d90d92f17eef5063fde33553314c7b8b83669c
William Gotterer <william.gotterer@gmail.com>
2021-10-11 13:09:29 -0400

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const headerStyle = {
+  color: "firebrick"
+}
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+    <div id="home">
+      <h1 style={headerStyle}>Your {name} is a Web Developer from Your {city}</h1>
+    </div>
+  )
+  
 }
 
 export default Home;
+///nothing renders when saved 
